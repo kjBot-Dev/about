7e1bb62d16e8a4dfd7160cec46d605a889b51781
LiangXiang Shen <kj415j45@gmail.com>
2019-01-09 15:55:45 +0800

    Create About.php

---
+++ About.php
@@ -0,0 +1,13 @@
+<?php
+namespace kjBotModule\kjBot\about;
+
+use kjBot\Framework\Module;
+use kjBot\Framework\Message;
+use kjBot\Framework\Event\MessageEvent;
+use kjBot\Framework\DataStorage;
+
+class Listen extends Module{
+    public function process(array $args, MessageEvent $event): Message{
+        return $event->sendBack(DataStorage::GetData('about.txt'));
+    }
+}
